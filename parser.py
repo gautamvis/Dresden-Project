2d78ee7847f94b272e5da2389a0933a3a01ab624
gautamvis <gautamv@umich.edu>
2016-06-21 10:56:52 +0200

    Add files via upload

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ filename = input('Enter filename: ')
 #Set up to parse
 import xml.etree.ElementTree as my_el_tree
 tree = my_el_tree.parse(filename)
-root = tree.getroot()
+#root = tree.getroot()
 
 #Parse xml file
 with open('outfile.vhdl', 'w+') as outfile:
@@ -16,34 +16,26 @@ with open('outfile.vhdl', 'w+') as outfile:
         'library ieee;\n'
         'use ieee.std_logic_1164.all;\n'
         'use ieee.numeric_std.all;\n'
-        , file=outfile)
+        ,file=outfile
+    )
 
-    #Write components of each block 
+    #Write data from each block in xml
     print('begin\n', file=outfile)
+    for block in tree.iter(tag='block'):
+
+        print("{blockname} : {blocktype}"
+              .format(blockname = block.get('name'), blocktype = block.get('instance_type')),
+              "\n port map( ", file=outfile
+        )                                                 
+
+        #Write data for each port in a block
+        for port in block:
+            print( "  {portname} => "
+                   .format(portname = port.get('name')),
+                   file=outfile
+            )
+            
+        print("\n", file=outfile)
+
+    print("end structural;\n", file=outfile)
     
-    for tempblock in tree.iter(tag='block'):
-        
-        #FIXME
-        print(' ',
-              tempblock.get('name'), ' : ',
-              tempblock.get('instance_type'),
-              '\n\tport map(\n',
-              file=outfile)
-
-        for temppost in tempblock:
-            print('\t', temppost.get('name'),
-                  '\t', temppost.get('type'),
-                  '\t', temppost.get('width'), '\n',
-                  file=outfile, end="")
-
-        print('\n', file=outfile)
-        
-    print('end structural\n', file=outfile)
-
-        
-        
-
-
-#print(infile.read(), file=outfile)
-
-        
